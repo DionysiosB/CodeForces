1ee78a6dad63e3c41c085e86e16403f60cb8b11b
Dionysios Barmpoutis <barmpoutis@gmail.com>
2015-12-07 05:58:29 -0500

    367A-SerejaAndAlgorithm

--- 367A-SerejaAndAlgorithm.cpp
+++ 367A-SerejaAndAlgorithm.cpp
@@ -1,24 +1,27 @@
 #include <cstdio>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 
 int main(){
 
     std::string input; getline(std::cin, input);
-    
+    std::vector<std::vector<long> > counts(1 + input.size(), std::vector<long>(3,0));
+    for(long p = 1; p <= input.size(); p++){
+        for(long q = 0; q < 3; q++){counts[p][q] = counts[p - 1][q] + (input[p - 1] == 'x' + q);}
+    }
+
     long m(0); scanf("%ld", &m);
     while(m--){
         long start(0), end(0); scanf("%ld %ld", &start, &end);
-        long numX(0), numY(0), numZ(0);
-        for(long k = start - 1; k < end; k++){
-            std::cout 
-            if(input[k] == 'x'){++numX;}
-            else if(input[k] == 'y'){++numY;}
-            else if(input[k] == 'z'){++numZ;}
-        }
-        long common = std::min(numX, std::min(numY, numZ));
-        if(numX + numY + numZ - 3 * common <= 2){puts("YES");}
-        else{puts("NO");}
+        std::vector<long> dist(3);
+        for(int q = 0; q < 3; q++){dist[q] = counts[end][q] - counts[start - 1][q];}
+        sort(dist.begin(), dist.end());
+        //std::cout << input.substr(start - 1, end - start + 1) << "\t";
+
+        if(end - start + 1 < 3 || dist[2] - dist[0] <= 1){puts("YES");} else{puts("NO");}
     }
     return 0;
 }
