145e9f3a773d5c3338de3c07bfbb80186aa99f28
Dionysios Barmpoutis <barmpoutis@gmail.com>
2018-01-04 06:16:41 -0500

    681C-HeapOperations-Old

---
+++ 681C-HeapOperations-Old.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+int main(){
+
+    std::ios_base::sync_with_stdio(false);
+    long n; std::cin >> n;
+    std::vector<std::string> op;
+    std::multiset<long> ms;
+    for(long p = 0; p < n; p++){
+        std::string s; std::cin >> s;
+        if(s == "insert"){
+            long x; std::cin >> x;
+            ms.insert(x);
+            op.push_back("insert " + std::to_string(x));
+        }
+        else if(s == "removeMin"){
+            if(ms.empty()){ms.insert(0); op.push_back("insert 0");}
+            ms.erase(ms.begin());
+            op.push_back(s);
+        }
+        else if(s == "getMin"){
+            long x; std::cin >> x;
+            while(!ms.empty() && ((*ms.begin()) < x)){
+                ms.erase(ms.begin());
+                op.push_back("removeMin");
+            }
+            if(ms.empty() || x < (*ms.begin())){
+                ms.insert(x);
+                op.push_back("insert " + std::to_string(x));
+            }
+
+            op.push_back("getMin " + std::to_string(x));
+        }
+    }
+
+    std::cout << op.size() << std::endl;
+    for(size_t p = 0; p < op.size(); p++){std::cout << op[p] << std::endl;}
+
+    return 0;
+}
