cfc05806d91d008446031ec6c4ab479a4c3a73d7
Dionysios Barmpoutis <barmpoutis@gmail.com>
2019-10-05 07:06:40 -0500

    1194A-RemoveAProgression

---
+++ 1194A-RemoveAProgression.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+ 
+int main(){
+    
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, x; scanf("%ld %ld", &n, &x);
+        printf("%ld\n", 2 * x);
+    }
+    
+    return 0;
+}
