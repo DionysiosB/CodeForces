884f0235cd4fb14d6c9b81f3ebb70bda55e8bbf3
Dionysios Barmpoutis <barmpoutis@gmail.com>
2019-10-29 05:59:00 -0500

    1029A-ManyEqualSubstrings

--- 1029A-ManyEqualSubstrings.cpp
+++ 1029A-ManyEqualSubstrings.cpp
@@ -5,19 +5,16 @@ int main(){
     long n, k; std::cin >> n >> k;
     std::string s; std::cin >> s;
 
-    long fold(s.size());
-    for(long d = 1; d < s.size(); p++){
-        bool possible(true);
-        for(long q = 0; q + d < s.size(); q++){
-            if(s[q] != s[q + d]){possible = false;}
-        }
-
-        if(possible){fold = d; break;}
+    long start(0);
+    for(long dist = 1; dist < s.size(); dist++){
+        long g(dist);
+        while(g < s.size()){if(s[g - dist] != s[g]){break;} ++g;}
+        if(g == s.size()){start = s.size() - dist; break;}
     }
 
-
-
-
+    std::cout << s;
+    for(long p = 1; p < k; p++){std::cout << s.substr(start);}
+    std::cout << std::endl;
 
     return 0;
 }
