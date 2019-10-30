eea14fe6c9f9eadadb1f88843b26a52fcbe44172
Dionysios Barmpoutis <barmpoutis@gmail.com>
2019-10-30 05:58:59 -0500

    1043B-LostArray

---
+++ 1043B-LostArray.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <vector>
+
+int main(){
+
+    long n; scanf("%ld", &n);
+    long prev(0);
+    std::vector<long> a(n); for(long p = 0; p < n; p++){scanf("%ld", &a[p]);}
+
+    return 0;
+}
