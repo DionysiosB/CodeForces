cb748b91f98fc94f8034f74a6e49197b9b40680e
Dionysios Barmpoutis <barmpoutis@gmail.com>
2020-01-13 18:06:36 -0600

    1252A-CopyingHomework

---
+++ 1252A-CopyingHomework.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(){
+
+    long n; scanf("%ld", &n);
+    for(long p = 0; p < n; p++){long x; scanf("%ld", &x); printf("%ld ", (n + 1 - x));}
+    puts("");
+
+    return 0;
+}
