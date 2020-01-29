53b6d0ca9f8d5449a6564349fed146678fe1b600
Dionysios Barmpoutis <barmpoutis@gmail.com>
2020-01-28 18:47:18 -0600

    1253C-SweetsEating

---
+++ 1253C-SweetsEating.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+typedef long long ll;
+
+int main(){
+
+    ll n, m; scanf("%lld %lld", &n, &m);
+    std::vector<ll> a(n + 1); for(ll p = 1; p <= n; p++){scanf("%lld", &a[p]);}
+
+    return 0;
+}
