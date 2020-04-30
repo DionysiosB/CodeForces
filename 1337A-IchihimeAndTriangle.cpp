6536d4d11cc4879f6ae3d056f12b4bd205847e0d
Dionysios Barmpoutis <barmpoutis@gmail.com>
2020-04-30 12:13:39 -0500

    1337A-IchihimeAndTriangle

---
+++ 1337A-IchihimeAndTriangle.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long a, b, c, d; scanf("%ld %ld %ld %ld", &a, &b, &c, &d);
+        printf("%ld %ld %ld\n", b, c, c);
+    }
+
+    return 0;
+}
