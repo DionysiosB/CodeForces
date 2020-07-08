f1e49ad51ece176057faeab2993e7438116ceec6
Dionysios Barmpoutis <barmpoutis@gmail.com>
2020-07-08 06:35:34 -0500

    1307B-CowAndFriend

--- 1307B-CowAndFriend.cpp
+++ 1307B-CowAndFriend.cpp
@@ -6,16 +6,14 @@ int main(){
     long t; scanf("%ld", &t);
     while(t--){
         long n, x; scanf("%ld %ld", &n, &x);
-        std::set<long> s; long mx(0);
+        long mx(0); bool found(false);
         for(long p = 0; p < n; p++){
-            long cur; scanf("%ld", &cur);
-            s.insert(cur);
-            mx = (mx > cur) ? mx : cur;
+            long z; scanf("%ld", &z); mx = (mx > z) ? mx : z;
+            if(z == x){found = true; break;}
         }
-
-        long cnt = s.count(x) ? ((x + mx - 1) / mx) : 1;
-        if(s.count(x)){cnt = 1;}
-        printf("%ld\n", cnt);
+        long u = (x + mx - 1) / mx;
+        u = (u > 2) ? u : 2;
+        printf("%ld\n", found ? 1 : u);
     }
 
     return 0;
