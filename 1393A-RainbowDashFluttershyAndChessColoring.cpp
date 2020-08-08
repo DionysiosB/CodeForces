fe21e25c5061d15e6788b2823e7f6c58f00c5e36
Dionysios Barmpoutis <barmpoutis@gmail.com>
2020-08-08 13:01:28 -0500

    Create 1393A-RainbowDashFluttershyAndChessColoring.cpp

---
+++ 1393A-RainbowDashFluttershyAndChessColoring.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long x; scanf("%ld", &x);
+        printf("%ld\n", 1 + x / 2);
+    }
+
+    return 0;
+}
