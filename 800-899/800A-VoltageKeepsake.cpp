e7cf6e2aa0774c38178103bdccdcca719f86fcd3
Yagnik Sakhiya <45854830+sakhiya07@users.noreply.github.com>
2020-10-01 01:03:36 +0530

    Update 800A-VoltageKeepsake.cpp

--- 800-899/800A-VoltageKeepsake.cpp
+++ 800-899/800A-VoltageKeepsake.cpp
@@ -36,3 +36,28 @@ int main(){
 
     return 0;
 }
+
+
+// #include<cstdio>
+// const int N=100005;
+// long long s;
+// int n,p,a[N],b[N],i;
+// double l,m,r,x,y,t;
+// int main(){
+//     scanf("%d%d",&n,&p);
+//     for(i=1;i<=n;++i)scanf("%d%d",a+i,b+i),s+=a[i];
+//     if(s<=p)return puts("-1"),0;l=0;r=1e18;
+//     for(;r-l>1e-7;){
+//         m=(l+r)/2;x=m*p;
+//         for(i=1;i<=n;++i){
+//             t=b[i]*1.0/a[i];
+//             if(t<m)x-=(m-t)*a[i];
+//             if(x<0)break;
+//         }
+//         if(i>n)l=m;
+//             else r=m;
+//     }
+// //     printf("%.5f\n",l);
+//     return 0;
+// }
+// short another solution
