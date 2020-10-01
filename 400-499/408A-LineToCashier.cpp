4fd37986344b2c0b9aa8b7e7dc7ecdde4f24f0e7
het262 <71756306+het262@users.noreply.github.com>
2020-10-02 03:35:50 +0530

    Update 408A-LineToCashier.cpp

--- 400-499/408A-LineToCashier.cpp
+++ 400-499/408A-LineToCashier.cpp
@@ -1,25 +1,47 @@
-#include <cstdio>
-#include <vector>
-
-int main(){
-
-    const int timePerItem = 5;
-    const int timePerCustomer = 15;
-
-    int n(0); scanf("%d", &n);
-    std::vector<int> numCustomers(n,0);
-    for(int k = 0; k < n; k++){scanf("%d", &numCustomers[k]);}
-
-    long minTime = 1000000000;
-    for(int k = 0; k < n; k++){
-        long totalTime = 0;
-        for(int m = 0; m < numCustomers[k]; m++){int temp; scanf("%d", &temp); totalTime += temp;}
-        totalTime *= timePerItem;
-        totalTime += timePerCustomer * numCustomers[k];
-        if(totalTime < minTime){minTime = totalTime;}
-    }
-
-    printf("%ld\n", minTime);
-
-    return 0;
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define ld long double
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+ 
+#define mp make_pair
+#define bp __builtin_popcountll
+#define MOD  998244353
+const int N = 200005;
+#define FAST ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);srand(time(NULL));
+ 
+void solve()
+{
+     ll ans = INT_MAX;
+     ll n;
+     cin >> n;
+     ll sz[n];
+     for(ll i=0;i<n;i++)   cin >> sz[i];
+     ll a[n][105];
+     for(ll i=0;i<n;i++)
+     {
+          for(ll j=0;j<sz[i];j++)    cin >> a[i][j];
+          ll sum = sz[i]*15;
+          for(ll j=0;j<sz[i];j++)
+          {
+               sum += a[i][j]*5;
+          }
+          ans = min(ans,sum);
+     }
+     cout<<ans;
+}
+ 
+int main()
+{
+     FAST;
+     ll t = 1;
+    // cin >> t;
+     while(t--)
+     {
+          solve();
+     }
 }
