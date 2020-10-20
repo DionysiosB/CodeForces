8621d1af48c40ae2a0ac7bc48a909e52377f659e
Dionysios Barmpoutis <barmpoutis@gmail.com>
2020-10-20 08:03:02 -0500

    Create 1421A-XORwice.cpp

---
+++ 1421A-XORwice.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long a, b; scanf("%ld %ld", &a, &b);
+        printf("%ld\n", a ^ b);
+    }
+
+    return 0;
+}
