c7776f2a1459c15e5c2ab03e34a8e5f7f2d8f2e1
Dionysios Barmpoutis <barmpoutis@gmail.com>
2020-11-21 08:20:34 -0600

    Create 1438A-SpecificTastesOfAndre.cpp

---
+++ 1438A-SpecificTastesOfAndre.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        for(long p = 0; p < n; p++){printf("1 ");}
+        puts("");
+    }
+
+    return 0;
+}
