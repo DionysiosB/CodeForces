a847664ddf84b6d709ec5157c718e580da9b589a
Dionysios Barmpoutis <barmpoutis@gmail.com>
2021-02-08 12:13:02 -0600

    Create PracticeTwoPointersMethod2A-SegmentWithSmallSum.cpp

---
+++ PracticeTwoPointersMethod2A-SegmentWithSmallSum.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <vector>
+typedef long long ll;
+
+int main(){
+
+    ll n, s; scanf("%lld %lld", &n, &s);
+    std::vector<ll> a(n, 0); for(ll p = 0; p < n; p++){scanf("%lld", &a[p]);}
+    ll left(0), cur(0), mx(0);
+    for(ll p = 0; p < n; p++){
+        cur += a[p];
+        while(cur > s){cur -= a[left]; ++left;}
+        ll len = p - left + 1;
+        mx = (mx > len) ? mx : len;
+    }
+
+    printf("%lld\n", mx);
+}
