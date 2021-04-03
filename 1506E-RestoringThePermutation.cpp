85c0b9e0cfd81fd1833eb3e0ac284f1b7c0b0e05
Dionysios Barmpoutis <barmpoutis@gmail.com>
2021-04-03 10:33:14 -0500

    Create 1506E-RestoringThePermutation.cpp

---
+++ 1506E-RestoringThePermutation.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <vector>
+
+long find(std::vector<long> &v, long upper){
+
+
+
+}
+
+
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        std::vector<long> v(n); for(long p = 0; p < n; p++){scanf("%ld", &v[p]);}
+
+        std::vector<long> avail(n + 1, 0); for(long p = 0; p <= n; p++){avail[p] = p;}
+        std::vector<long> ans(n, -1);
+        for(long p = 0; p < n; p++){
+            if(p == 0 || v[p] > v[p - 1]){upper = v[p];}
+            ans[p] = find(last);
+        }
+
+
+        for(long p = 0; p < n; p++){printf("%ld ", ans[p]);}
+        puts("");
+    }
+
+}
