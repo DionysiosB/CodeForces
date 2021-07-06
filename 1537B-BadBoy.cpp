8dfe6e55725611de96adbc2c3cd8b4afe6199b84
Dionysios Barmpoutis <barmpoutis@gmail.com>
2021-07-06 06:34:10 -0500

    Create 1537B-BadBoy.cpp

---
+++ 1537B-BadBoy.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, m, i, j; scanf("%ld %ld %ld %ld", &n, &m, &i, &j);
+        printf("1 %ld %ld 1\n", m, n);
+    }
+
+}
