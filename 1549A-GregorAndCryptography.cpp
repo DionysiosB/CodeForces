24509cb5a7188d3c2a6f01adcd1a31e6b8f3b912
Dionysios Barmpoutis <barmpoutis@gmail.com>
2021-08-28 16:25:24 -0500

    Create 1549A-GregorAndCryptography.cpp

---
+++ 1549A-GregorAndCryptography.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+     long t; scanf("%ld", &t);
+     while(t--){
+         long p; scanf("%ld", &p);
+         printf("%ld %ld\n", (p - 1) / 2, (p - 1));
+     }
+
+}
