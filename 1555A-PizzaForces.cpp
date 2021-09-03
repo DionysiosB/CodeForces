6da4fb39f9ea54338566dc3024c59488ea731f7e
Dionysios Barmpoutis <barmpoutis@gmail.com>
2021-09-03 06:47:46 -0500

    Update 1555A-PizzaForces.cpp

--- 1555A-PizzaForces.cpp
+++ 1555A-PizzaForces.cpp
@@ -8,28 +8,10 @@ typedef long long ll;
 int main(){
 
     ll t; scanf("%lld", &t);
-    std::vector<ll> time(120, 300);
-    for(ll a = 0; a <= 20; a++){
-        for(ll b = 0; b <= 15; b++){
-            for(ll c = 0; c <= 12; c++){
-                ll slices = 6 * a + 8 * b + 10 * c;
-                if(slices >= 120){continue;}
-                ll dur = 15 * a + 20 * b + 25 * c;
-                time[slices] = time[slices] < dur ? time[slices] : dur;
-            }
-        }
-    }
-
-    ll cur(time.back());
-    for(long p = time.size() - 1; p > 0; p--){
-        cur = (cur < time[p]) ? cur : time[p];
-        time[p] = cur;
-    }
-    time[0] = 0;
-
     while(t--){
         ll n; scanf("%lld", &n);
-        ll res = (n / 120) * 300 + time[n % 120];
+        n = (n > 6) ? n : 6;
+        ll res = (n + 1) / 2 * 5;  //if odd, make it even;
         printf("%lld\n", res);
     }
 
