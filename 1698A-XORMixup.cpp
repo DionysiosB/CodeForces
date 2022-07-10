b37118503f5057defaa71104e849e2fb1b3d2d45
Dionysios Barmpoutis <barmpoutis@gmail.com>
2022-07-10 08:05:04 -0500

    Create 1698A-XORMixup.cpp

---
+++ 1698A-XORMixup.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+ 
+int main(){
+ 
+  long t; scanf("%ld", &t);
+  while(t--){
+    long n; scanf("%ld", &n);
+    long res(0);
+    while(n--){scanf("%ld", &res);}
+    printf("%ld\n", res);
+  }
+  
+}
