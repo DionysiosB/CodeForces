c55057433fd1f271012c4d3212bf2b20a1f2af51
Dionysios Barmpoutis <barmpoutis@gmail.com>
2022-08-02 06:27:53 -0500

    Create 1711A-PerfectPermutation.cpp

---
+++ 1711A-PerfectPermutation.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        for(long p = 0; p < n; p++){printf("%ld ", 1 + ((p + 1) % n));}
+        puts("");
+    }
+
+}
