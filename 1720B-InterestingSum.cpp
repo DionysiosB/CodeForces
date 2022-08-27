d17b786ed5848bb76a53e3e90112f58ccaffb77b
Dionysios Barmpoutis <barmpoutis@gmail.com>
2022-08-27 07:54:40 -0500

    Create 1720B-InterestingSum.cpp

---
+++ 1720B-InterestingSum.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        std::vector<long> a(n); for(long p = 0; p < n; p++){scanf("%ld", &a[p]);}
+        sort(a.begin(), a.end());
+        long ans = a[n - 1] + a[n - 2] - a[0] - a[1];
+        printf("%ld\n", ans);
+    }
+
+}
