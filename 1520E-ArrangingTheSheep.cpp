01494bc23f603123f8ef2d85ab2bdf5aab6214d4
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-01-23 06:32:04 -0600

    Update 1520E-ArrangingTheSheep.cpp

--- 1520E-ArrangingTheSheep.cpp
+++ 1520E-ArrangingTheSheep.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <vector>
+typedef long long ll;
 
 int main(){
 
     std::ios_base::sync_with_stdio(false);
-    long t; std::cin >> t;
+    ll t; std::cin >> t;
     while(t--){
+        long n; std::cin >> n;
         std::string s; std::cin >> s;
-        std::vector<long> v;
-        for(long p = 0; p < s.size(); p++){if(s[p] == '*'){v.push_back(p);}}
-        long vs(v.size()); long med = (v[(vs - 1) / 2] + v[vs / 2]) / 2;
-        long long cnt(0);
-        long cur(0);
-        for(long p = med; p >= 0; p){
-            if(s[p] == '.'){;}
-            
-
+        std::vector<ll> v;
+        for(ll p = 0; p < s.size(); p++){if(s[p] == '*'){v.push_back(p);}}
+        if(v.size() <= 0){std::cout << "0" << std::endl; continue;}
+        ll middle = v.size() / 2;
+        ll medpos = v[middle];
+        ll total(0); 
+        for(ll p = 0; p < v.size(); p++){
+            ll target = medpos + (p - middle);
+            ll dist = v[p] - target; if(dist < 0){dist = -dist;}
+            total += dist;
         }
+
+        std::cout << total << std::endl;
     }
 
 }
