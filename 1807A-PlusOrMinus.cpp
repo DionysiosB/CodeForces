605c2b9c610b0516f262cfb33b2be5280be94c26
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-03-26 09:14:18 -0500

    Create 1807A-PlusOrMinus.cpp

---
+++ 1807A-PlusOrMinus.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long a, b, c; scanf("%ld %ld %ld", &a, &b, &c);
+        puts((a + b == c) ? "+" : "-");
+    }
+
+}
