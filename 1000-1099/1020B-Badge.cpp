4d7b40b5faa90d3c154299a2eb338417044eba4b
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-05-19 18:50:19 -0500

    1000-1099

--- 1000-1099/1020B-Badge.cpp
+++ 1000-1099/1020B-Badge.cpp
@@ -1,25 +1,16 @@
 #include <cstdio>
 #include <vector>
+#include <set>
 
 int main(){
 
     long n; scanf("%ld", &n);
     std::vector<long> a(n + 1); for(long p = 1; p <= n; p++){scanf("%ld", &a[p]);}
-    std::vector<bool> visited(n + 1, 0);
-    std::vector<long> b(n + 1, 0);
     for(long p = 1; p <= n; p++){
-        if(visited[p]){continue;}
-        long cur = p; std::vector<long> s;
-        while(!visited[cur]){s.push_back(cur); visited[cur] = true; cur = a[cur];}
-        bool found(false);
-        while(!s.empty()){
-            long u = s.back(); s.pop_back();
-            b[u] = found ? cur : u;
-            found = (u == cur);
-        }
+        std::set<long> s; 
+        long cur = p; 
+        while(s.count(cur) == 0){s.insert(cur); cur = a[cur];}
+        printf("%ld ", cur);
     }
-
-    for(long p = 1; p <= n; p++){printf("%ld ", b[p]);}; puts("");
-
-    return 0;
+    puts("");
 }
