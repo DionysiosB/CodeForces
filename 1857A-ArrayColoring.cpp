2136b02c4fb3d241d6c5acfddd85bcefe05977da
Dionysios Barmpoutis <barmpoutis@gmail.com>
2023-08-08 06:48:44 -0500

    Create 1857A-ArrayColoring.cpp

---
+++ 1857A-ArrayColoring.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld",&n);
+        long s(0); for(long p = 0; p < n; p++){long x; scanf("%ld", &x); s += x;}
+        puts(s % 2 ? "NO" : "YES");
+    }
+
+}
