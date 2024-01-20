addcb87af96300228494f147f4dd4f138c4f4eec
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-01-20 09:09:18 -0600

    1253C-SweetsEating.cpp

--- 1200-1299/1253C-SweetsEating.cpp
+++ 1200-1299/1253C-SweetsEating.cpp
@@ -6,7 +6,15 @@ typedef long long ll;
 int main(){
 
     ll n, m; scanf("%lld %lld", &n, &m);
-    std::vector<ll> a(n + 1); for(ll p = 1; p <= n; p++){scanf("%lld", &a[p]);}
+    std::vector<ll> a(n); for(ll p = 0; p <= n; p++){scanf("%lld", &a[p]);}
+    sort(a.begin(), a.end());
 
-    return 0;
+    ll cs(0); std::vector<ll> v(n);
+    for(long p = 0; p < n; p++){
+        cs += a[p];
+        v[p] = cs + ((p >= m) ? v[p - m] : 0);
+        printf("%lld ", v[p]);
+    }
+    puts("");
+
 }
