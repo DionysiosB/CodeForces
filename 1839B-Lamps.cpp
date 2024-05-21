6fb1589ffa49e6a44310c78b943ed0a931970219
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-05-21 00:26:49 -0500

    Update 1839B-Lamps.cpp

--- 1839B-Lamps.cpp
+++ 1839B-Lamps.cpp
@@ -1,16 +1,32 @@
 #include <cstdio>
 #include <algorithm>
 #include <vector>
+typedef long long ll;
 
-bool compare(request first, request second){return first.sum > second.sum;}
+bool cmp(std::pair<ll, ll> a, std::pair<ll, ll> b){
+    if(a.first != b.first){return a.first < b.first;}
+    return a.second > b.second;
+}
 
 int main(){
 
-    long t; scanf("%ld", &t);
+    ll t; scanf("%lld", &t);
     while(t--){
-        long n; scanf("%ld", &n);
-        std::vector<std::pair<long, long> > v(n);
-        for(long p = 0; p < n; p++){scanf("%ld %ld", &v[p].first, &v[p].second);}
+        ll n; scanf("%lld", &n);
+        std::vector<std::pair<ll, ll> > v(n);
+        for(ll p = 0; p < n; p++){scanf("%lld %lld", &v[p].first, &v[p].second);}
+        sort(v.begin(), v.end(), cmp);
+
+        ll idx(0), cnt(0), total(0);
+        for(ll p = 0; p < n; p++){
+            if(p < idx){continue;}
+            total += v[p].second;
+            ++cnt;
+            while(idx < n && v[idx].first <= cnt){++idx;}
+            cnt = p - idx + 1; cnt = (cnt > 0 ? cnt : 0);
+        }
+
+        printf("%lld\n", total);
     }
 
 }
