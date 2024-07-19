67f8ca0f29d78890b68e8e4c86ab8f2d40c06b9b
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-07-19 08:25:09 -0500

    Create 1983A-ArrayDivisibility.cpp

---
+++ 1983A-ArrayDivisibility.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        for(long p = 1; p <= n; p++){printf("%ld ", p);}
+        puts("");
+    }
+
+}
