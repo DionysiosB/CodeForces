87537f53020a4222b6cdfa737d006c32d049eaa2
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-07-23 06:48:21 -0500

    Update 1986C-UpdateQueries.cpp

--- 1986C-UpdateQueries.cpp
+++ 1986C-UpdateQueries.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
 
 int main(){
@@ -10,20 +11,21 @@ int main(){
     while(t--){
         long n, m; std::cin >> n >> m;
         std::string s; std::cin >> s;
-        std::vector<long> va(m); for(long p = 0; p < m; p++){std::cin >> va[p]; --va[p];}
-        std::string u; std::cin >> u;
-        sort(va.begin(), va.end());
-        sort(u.begin(), u.end());
-
+        std::set<long> t;
         for(long p = 0; p < m; p++){
-            long idx = va[p];
-            if(!vb[idx]){vb[idx] = 1; s[idx] = u[p];}
-            else if(u[p] < s[idx]){s[idx] = u[p];}
+            long x; std::cin >> x;
+            t.insert(--x);
+        }
+        std::string r; std::cin >> r;
+        sort(r.begin(), r.end());
+
+        long idx(0);
+        for(std::set<long>::iterator it = t.begin(); it != t.end(); it++){
+            long pos = *it;
+            s[pos] = r[idx++];
         }
 
         std::cout << s << std::endl;
     }
-
-
 
 }
