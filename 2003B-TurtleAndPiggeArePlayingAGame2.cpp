ea0cffa6bc763c66e9240bdb4d7a2302c0609f99
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-09-07 05:11:26 -0500

    Create 2003B-TurtleAndPiggeArePlayingAGame2.cpp

---
+++ 2003B-TurtleAndPiggeArePlayingAGame2.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        std::vector<long> a(n); for(long p = 0; p < n; p++){scanf("%ld", &a[p]);}
+        sort(a.begin(), a.end());
+        printf("%ld\n", a[n / 2]);
+    }
+
+}
