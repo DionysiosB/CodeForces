63300ce7510d0be8bef0687ffe8e9661618cb64f
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-09-10 13:03:02 -0500

    1000-1100

--- 1000-1099/1075B-TaxiDriversAndLyft.cpp
+++ 1000-1099/1075B-TaxiDriversAndLyft.cpp
@@ -6,13 +6,12 @@ int main(){
 
     long n, m; scanf("%ld %ld", &n, &m);
     std::vector<long> x(n + m); for(long p = 0; p < n + m; p++){scanf("%ld", &x[p]);}
-    std::vector<long> d(m), r(n), cnt(m, 0);
+    std::vector<long> d, r, cnt(m, 0);
 
-    long a(0), b(0);
     for(long p = 0; p < m + n; p++){
-        long y; scanf("%ld", &y);
-        if(y){d[a++] = x[p];}
-        else{r[b++] = x[p];}
+        int y; scanf("%d", &y);
+        if(y){d.push_back(x[p]);}
+        else{r.push_back(x[p]);}
     }
 
     sort(r.begin(), r.end());
@@ -21,9 +20,9 @@ int main(){
     long idx(0);
     for(long p = 0; p < n; p++){
         if(r[p] < d[0]){++cnt[0];}
-        else if(r[p] > d[m - 1]){++cnt[m - 1];}
+        else if(r[p] > d.back()){++cnt.back();}
         else{
-            if(r[p] > d[idx]){++idx;}
+            while(idx + 1 < d.size() && r[p] > d[idx]){++idx;}
             if(r[p] - d[idx - 1] <= d[idx] - r[p]){++cnt[idx - 1];}
             else{++cnt[idx];}
         }
