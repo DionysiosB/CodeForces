031989a292201f6a7d0d97cee7a9e0414bdb7444
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-09-10 13:04:21 -0500

    1400-1499

--- 1400-1499/1480B-TheGreatHero.cpp
+++ 1400-1499/1480B-TheGreatHero.cpp
@@ -1,25 +1,23 @@
 #include <cstdio>
 #include <vector>
-#include <algorithm>
+typedef long long ll;
 
 int main(){
 
     long t; scanf("%ld", &t);
     while(t--){
-        long A, B, n; scanf("%ld %ld %ld", &A, &B, &n);
-        std::vector<long> a(n); for(long p = 0; p < n; p++){scanf("%ld", &a[p]);}
-        std::vector<long> b(n); for(long p = 0; p < n; p++){scanf("%ld", &b[p]);}
-        std::vector<long> d(n); for(long p = 0; p < n; p++){d[p] = ((b[p] - 1) / A) * a[p];}
-        //for(long p = 0; p < n; p++){printf("B:%ld d[%ld]:%ld\n", B, p, d[p]);}
-        for(long p = 0; p < n; p++){B -= d[p];}
-        sort(a.begin(), a.end());
-        bool res(B > 0);
-        for(long p = 0; p < n; p++){
-            if(B <= 0){res = false; break;}
-            B -= a[p];
+        ll A, B, n; scanf("%lld %lld %lld", &A, &B, &n);
+        std::vector<ll> a(n); for(ll p = 0; p < n; p++){scanf("%lld", &a[p]);}
+        std::vector<ll> b(n); for(ll p = 0; p < n; p++){scanf("%lld", &b[p]);}
+
+        ll mxd(0);
+        for(ll p = 0; p < n; p++){
+            ll r = (b[p] + A - 1) / A;
+            B -= r * a[p];
+            mxd = (mxd > a[p] ? mxd : a[p]); 
         }
 
-        puts(res ? "YES" : "NO");
+        puts(B + mxd > 0 ? "YES" : "NO");
     }
 
 }
