01a738212a50671a268a0b62e3833dbb5a87cd36
Dionysios Barmpoutis <barmpoutis@gmail.com>
2024-09-22 07:30:17 -0500

    Create 2005B1-TheStrictTeacherHardVersion.cpp

---
+++ 2005B1-TheStrictTeacherHardVersion.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, m, q; scanf("%ld %ld %ld", &n, &m, &q);
+        std::vector<long> b(m); for(long p = 0; p < m; p++
+
+    }
+}
