b8b90664202847ac94d2539549aa4be43567f722
Dionysios Barmpoutis <barmpoutis@gmail.com>
2025-01-26 11:29:32 -0600

    Update 2060A-Fibonacciness.cpp

--- 2060A-Fibonacciness.cpp
+++ 2060A-Fibonacciness.cpp
@@ -5,9 +5,12 @@ int main(){
     long t; scanf("%ld", &t);
     while(t--){
         long a, b, d, e; scanf("%ld %ld %ld %ld", &a, &b, &d, &e);
-        int fa = (d == a + 2 * b) + (e == 2 * a + 3 * b);
-        int fb = (e == a + b + d) + (2 * d == b + e);
-        printf("%d\n", 1 + (fa > fb ? fa : fb));
+        int f1 = (d == a + 2 * b) + (e == a + b + d);
+        int f2 = (d == a + 2 * b) + (2 * d == b + e);
+        int f3 = (e == a + b + d) + (2 * d == b + e);
+        int f = (f1 > f2 ? f1 : f2);
+        f = (f > f3 ? f : f3);
+        printf("%d\n", 1 + f);
     }
 
 }
