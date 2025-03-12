2d08e859ef85ffce1dab44460d54f39450f79ac1
Dionysios Barmpoutis <barmpoutis@gmail.com>
2025-03-12 07:03:46 -0500

    Create 2074B-TheThirdSide.cpp

---
+++ 2074B-TheThirdSide.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        long s(0);
+        for(long p = 0; p < n; p++){long x; scanf("%ld", &x); s += x;}
+        printf("%ld\n", s - (n - 1));
+    }
+
+}
