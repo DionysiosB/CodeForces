3e62104d2a5174a1736849a0001af2c1783138d7
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-04-26 15:01:33 -0500

    Create 2098B-SashaAndTheApartmentPurchase.cpp

---
+++ 2098B-SashaAndTheApartmentPurchase.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <vector>
+
+int main(){
+
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n, k; scanf("%ld %ld", &n, &k);
+        std::vector<long> a(n); for(long p = 0; p < n; p++){scanf("%ld", &a[p]);}
+        sort(a.begin(), a.end());
+        printf("%ld\n", (a[k + (n - k) / 2] - a[(n - k - 1) / 2]) + 1);
+    }
+
+}
