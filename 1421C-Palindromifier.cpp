3f309534149dfbf93dda4dfa0660ac7e72143ea4
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-06-26 06:49:47 -0500

    Create 1421C-Palindromifier.cpp

---
+++ 1421C-Palindromifier.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    std::string s; std::cin >>  s;
+    std::cout << 3 << std::endl;
+    std::cout << "R " << (s.size() - 1) << std::endl;
+    std::cout << "L " << s.size() << std::endl;
+    std::cout << "L 2" << std::endl;
+}
+
