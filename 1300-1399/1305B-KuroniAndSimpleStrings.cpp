3728b80ac51ac224b9403e7cb768d60a9eb90d7e
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-06-27 07:18:09 -0500

    1300-1399

--- 1300-1399/1305B-KuroniAndSimpleStrings.cpp
+++ 1300-1399/1305B-KuroniAndSimpleStrings.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main(){
 
+    std::ios_base::sync_with_stdio(false);
     std::string s; std::cin >> s;
-    bool left(false); int action(0);
-    long cnt(0);
+
+    std::vector<long> v;
+    long idx(s.size());
     for(long p = 0; p < s.size(); p++){
-        if(s[p] == '('){left = true;}
-        else if(s[p] == ')' && left){action = 1;}
-        cnt += (s[p] == ')');
+        if(s[p] == ')'){continue;}
+        while(idx > p){
+            if(s[idx] == ')'){v.push_back(1 + p); v.push_back(1 + idx--); break;}
+            --idx;
+        }
     }
 
-    std::cout << action << std::endl;
-    if(action){
-        std::cout << cnt << std::endl;
-        for(long p = 0; p < s.size(); p++){if(s[p] == ')'){std::cout << (p + 1) << " ";}}
+    sort(v.begin(), v.end());
+    if(v.empty()){std::cout << "0" << std::endl;}
+    else{
+        std::cout << 1 << std::endl << v.size() << std::endl;
+        for(long p = 0; p < v.size(); p++){std::cout << v[p] << " ";}
         std::cout << std::endl;
     }
     
-    return 0;
 }
