0ab2281f299d6b64b9d2734854eb6792747c48b2
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-06-27 07:18:53 -0500

    1500-1599

--- 1500-1599/1516B-AGAGAXOOORRR.cpp
+++ 1500-1599/1516B-AGAGAXOOORRR.cpp
@@ -1,11 +1,20 @@
 #include <cstdio>
+#include <vector>
  
 int main(){
     long t; scanf("%ld", &t);
     while(t--){
         long n; scanf("%ld", &n);
-        long cx(0);
-        for(long p = 0; p < n; p++){long x; scanf("%ld", &x); cx ^= x;}
-        puts(cx ? "NO" : "YES");
+        std::vector<long> v(n, 0);
+        for(long p = 0; p < n; p++){long x; scanf("%ld", &x); v[p] = (p ? v[p - 1] : 0) ^ x;}
+
+        bool possible(!v.back());
+        for(long p = 0; !possible && p < n; p++){
+            for(long q = p + 1; !possible && q < n; q++){
+                possible |=  (v[p] == (v[q] ^ v[p]))  && (v[p] == (v.back() ^ v[q])); 
+            }
+        }
+
+        puts(possible ? "YES" : "NO");
     }
 }
