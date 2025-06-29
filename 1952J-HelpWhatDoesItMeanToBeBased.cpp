0dc648c0937b4ae9e40f1383fecda3d4ce121b50
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-06-29 05:47:43 -0500

    Create 1952J-HelpWhatDoesItMeanToBeBased.cpp

---
+++ 1952J-HelpWhatDoesItMeanToBeBased.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    int n; std::cin >> n;
+    if(n == 1){std::cout<<"yoink a\nyoink b\n*slaps a on top of b*\nyeet b\ngo touch some grass\n";}
+    else if(n == 2){std::cout << "yoink a\nbruh b is lowkey just 0\nrip this b fell off by a\nvibe check a ratios b\nsimp for 7\nbruh a is lowkey just b\nyeet a\ngo touch some grass\n";}
+    else if(n == 3){std::cout<<"yoink n\nyoink a\nbruh m is lowkey just a[0]\nbruh i is lowkey just 1\nvibe check n ratios i\nsimp for 9\nyeet m\ngo touch some grass\nvibe check a[i] ratios m\nbruh m is lowkey just a[i]\n*slaps 1 on top of i*\nsimp for 5\n";}
+    else if(n == 4){std::cout<<"yoink n\nyoink a\nyoink k\nbruh i is lowkey just 0\nvibe check n ratios i\nsimp for 10\n*slaps -1 on top of k*\nyeet a[k]\ngo touch some grass\nbruh j is lowkey just 0\nvibe check n ratios j\nsimp for 15\n*slaps 1 on top of i*\nsimp for 5\nvibe check a[i] ratios a[j]\nsimp for 19\n*slaps 1 on top of j*\nsimp for 11\nbruh l is lowkey just a[i]\nbruh a[i] is lowkey just a[j]\nbruh a[j] is lowkey just l\nsimp for 17\n";}
+
+}
