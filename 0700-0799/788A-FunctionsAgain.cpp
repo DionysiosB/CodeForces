8d4781a0ab1b4345893ca616477a36900fd751c4
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-09-10 06:44:21 -0500

    0700-0799

--- 0700-0799/788A-FunctionsAgain.cpp
+++ 0700-0799/788A-FunctionsAgain.cpp
@@ -1,31 +1,36 @@
 #include <cstdio>
 #include <vector>
+typedef long long ll;
 
 int main(){
 
-    long n; scanf("%ld", &n);
-    std::vector<long> a(n); for(long p = 0; p < n; p++){scanf("%ld", &a[p]);}
-    std::vector<long> d(n - 1); for(long p = 0; p < n - 1; p++){
-        d[p] = a[p + 1] - a[p];
-        if(d[p] < 0){d[p] = -d[p];}
-    }
-    for(long p = 0; p < n - 1; p++){printf("%ld ", d[p]);}; puts("");
-
-    long mx(d[0]), cur(d[0]);
-    for(long p = 2; p < n - 1; p += 2){
-        cur += d[p] - d[p - 1];
-        if(cur < 0){cur = d[p];}
-        mx = (mx > cur) ? mx : cur;
+    ll n; scanf("%lld", &n);
+    long prev; scanf("%ld", &prev);
+    std::vector<ll> d(n - 1);
+    for(ll p = 0; p + 1 < n; p++){
+        ll a; scanf("%lld", &a);
+        d[p] = a - prev;
+        d[p] = (d[p] > 0 ? d[p] : -d[p]);
+        prev = a;
     }
 
-    cur = d[1]; mx = (mx > d[1]) ? mx : d[1];
-    for(long p = 3; p < n - 1; p += 2){
-        cur += d[p] - d[p - 1];
-        if(cur < 0){cur = d[p];}
-        mx = (mx > cur) ? mx : cur;
+    //f,g are the current sums where the last addition is positive/negative respectively
+    //if g is negative, there is no reason to add it to the next sum
+    ll fprev(0), gprev(0), mx(0);
+    for(ll p = 0; p < n - 1; p++){
+        ll f = gprev + d[p];
+
+        ll g = fprev - d[p];
+        g = (g > 0 ? g : 0);
+
+        mx = (mx > f ? mx : f);
+        mx = (mx > g ? mx : g);
+
+        fprev = f;
+        gprev = g;
     }
 
-    printf("%ld\n", mx);
+    printf("%lld\n", mx);
 
     return 0;
 }
