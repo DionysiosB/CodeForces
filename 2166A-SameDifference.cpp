517237c82b0acd8478b854e875e0e78a862266ad
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2025-11-17 06:55:36 -0600

    Create 2166A-SameDifference.cpp

---
+++ 2166A-SameDifference.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    std::ios_base::sync_with_stdio(false);
+    long t; std::cin >> t;
+    while(t--){
+        long n; std::cin >> n;
+        std::string s; std::cin >> s;
+        long cnt(0);
+        for(char x : s){cnt += (x != s.back());}
+        std::cout << cnt << std::endl;
+    }
+
+}
