53865525e24c6a7e36ebcaf37bf02f125680e089
Dionysios Barmpoutis <dbarmpoutis@gmail.com>
2026-01-14 12:28:19 -0600

    Create 2184A-SocialExperiment.cpp

---
+++ 2184A-SocialExperiment.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+ 
+int main() {
+    
+    long t; scanf("%ld", &t);
+    while(t--){
+        long n; scanf("%ld", &n);
+        printf("%ld\n", (n <= 3 ? n : (n % 2)));
+    }
+}
