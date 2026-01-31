cad90b6ddca0c655ccc6417922ff8e4572316594
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2026-01-31 13:19:15 -0600

    Update 2182C-ProductionOfSnowmen.cpp

--- 2182C-ProductionOfSnowmen.cpp
+++ 2182C-ProductionOfSnowmen.cpp
@@ -1,15 +1,15 @@
 #include <cstdio>
 #include <vector>
- 
+
 int main(){
- 
+
     long t; scanf("%ld", &t);
     while(t--){
         long n; scanf("%ld", &n);
         std::vector<long> a(n); for(long &x : a){scanf("%ld", &x);}
         std::vector<long> b(n); for(long &x : b){scanf("%ld", &x);}
         std::vector<long> c(n); for(long &x : c){scanf("%ld", &x);}
- 
+
         long long cnt(0);
         for(long bs = 0; bs < n; bs++){
             bool valid(true);
@@ -18,7 +18,7 @@ int main(){
             }
             cnt += n * valid;
         }
- 
+
         long prev(cnt); cnt = 0;
         for(long cs = 0; cs < n; cs++){
             bool valid(true);
@@ -27,8 +27,8 @@ int main(){
             }
             cnt += prev * valid;
         }
- 
+
         printf("%lld\n", cnt);
     }
- 
+
 }
