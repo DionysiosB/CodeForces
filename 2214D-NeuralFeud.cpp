d8c6879ff95207db63b2fefaa14d78547d617ca7
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2026-04-05 07:32:34 -0400

    Create 2214D-NeuralFeud.cpp

---
+++ 2214D-NeuralFeud.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+ 
+int main(){
+
+    int q; scanf("%d", &q);
+    if(q == 1){puts("walk");}
+    else if(q == 2){puts("no");}
+    else if(q == 3){puts("no");}
+    else if(q == 4){puts("no");}
+    else if(q == 5){puts("yes");}
+    else if(q == 6){puts("yes");}
+    else if(q == 7){puts("backwards");}
+    else if(q == 8){puts("seven");}
+}
