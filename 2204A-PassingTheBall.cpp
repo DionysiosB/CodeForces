7121b457d51c6314efd0e8686b59de252eaadde2
Dionysios Barmpoutis <4016051+DionysiosB@users.noreply.github.com>
2026-05-17 08:42:00 -0400

    Create 2204A-PassingTheBall.cpp

---
+++ 2204A-PassingTheBall.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    std::ios_base::sync_with_stdio(false);
+
+    long t; std::cin >> t;
+    while(t--){
+        long n; std::cin >> n;
+        std::string s; std::cin >> s;
+        long cnt(0);
+        for(long p = 0; s[p] == 'R' && p < s.size(); p++, cnt = p + 1);
+        std::cout << cnt << std::endl;
+    }
+
+}
